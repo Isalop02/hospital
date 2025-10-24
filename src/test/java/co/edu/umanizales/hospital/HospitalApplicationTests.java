5681b19682ab29217a01c17b1c510c9f0a95c343
Isabela <ilopez116249@umanizales.edu.co>
2025-10-23 20:06:16 -0500

    mi primer API hospital con saludo hola campeones

---
+++ src/test/java/co/edu/umanizales/hospital/HospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
